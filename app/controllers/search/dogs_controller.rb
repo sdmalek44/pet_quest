eac0dc92164137b98840f2b2ac051085b5669399
Stephen Malek <malek.stephen44@gmail.com>
2018-09-10 13:19:07 -0600

    move dog search logic into presenter

--- app/controllers/search/dogs_controller.rb
+++ app/controllers/search/dogs_controller.rb
@@ -1,21 +1,6 @@
 class Search::DogsController < ApplicationController
 
   def index
-    if search_params[:breed]
-      query = search_params.to_h.inject("") do |collector, (key, value)|
-        collector += "#{key}=#{value}&" unless value.empty?
-        collector
-      end.chop!
-
-      conn = Faraday.new(url: "http://api.petfinder.com") { |faraday| faraday.adapter Faraday.default_adapter }
-      response = conn.get("/pet.find?key=#{ENV['PET_FINDER_TOKEN']}&animal=dog&location=80209&#{query}&format=json&output=full")
-      dogs_info = JSON.parse(response.body, symbolize_names: true)[:petfinder][:pets][:pet]
-
-      @dogs = dogs_info.map do |dog_info|
-        Dog.new(dog_info)
-      end
-    end
-
   end
 
   def show
@@ -32,7 +17,7 @@ class Search::DogsController < ApplicationController
   private
 
   def presenter
-    @presenter ||= DogSearchPresenter.new(current_user)
+    @presenter ||= DogSearchPresenter.new(current_user, search_params)
   end
 
   def search_params
--- app/presenters/dog_search_presenter.rb
+++ app/presenters/dog_search_presenter.rb
@@ -1,6 +1,21 @@
 class DogSearchPresenter < BasePresenter
-  def initialize(user)
+  def initialize(user, param_info = {})
+    @param_info = param_info.to_h
     super(user)
   end
 
+  def build_query
+    @param_info.inject("") do |collector, (key, value)|
+      collector += "#{key}=#{value}&" unless value.empty?
+      collector
+    end.chop!
+  end
+
+  def dogs
+    conn = Faraday.new(url: "http://api.petfinder.com") { |faraday| faraday.adapter Faraday.default_adapter }
+    response = conn.get("/pet.find?key=#{ENV['PET_FINDER_TOKEN']}&animal=dog&location=80209&#{build_query}&format=json&output=full")
+    dogs_info = JSON.parse(response.body, symbolize_names: true)[:petfinder][:pets][:pet]
+
+    dogs_info.map { |dog_info| Dog.new(dog_info) }
+  end
 end
