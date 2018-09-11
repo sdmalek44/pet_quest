4932b1e597b78822b59e0a2f399e55254b4e2fc9
Stephen Malek <malek.stephen44@gmail.com>
2018-09-10 22:25:27 -0600

    add default photo if cat not found

--- app/models/null_photo_album.rb
+++ app/models/null_photo_album.rb
@@ -1,9 +1,9 @@
 class NullPhotoAlbum
   def thumbnail_photo
-    "https://vignette.wikia.nocookie.net/max-steel-reboot/images/7/72/No_Image_Available.gif/revision/latest?cb=20130902173013"
+    "http://photos.petfinder.com/photos/pets/42615260/1/?bust=1535959630&width=500&-x.jpg"
   end
 
   def profile_photo
-    "http://www.citymall.co.tz/uploads/no_image.png"
+    "http://photos.petfinder.com/photos/pets/42615260/1/?bust=1535959630&width=500&-x.jpg"
   end
 end
