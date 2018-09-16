9fe4f6c9496c7a7183827f7605f39148e578c0df
Stephen Malek <malek.stephen44@gmail.com>
2018-09-16 16:10:56 -0600

    make test work before each was the problem

--- spec/features/user_can_add_and_remove_favorite_spec.rb
+++ spec/features/user_can_add_and_remove_favorite_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 describe 'registered user' do
-  before(:each) do
-    @user = create(:user)
-    @favorite = create(:favorite, user_id: @user.id)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-  end
   context 'visits /search/dogs' do
     it 'can click a button on a dog that adds it to favorites' do
+      user = create(:user)
+      favorite = create(:favorite, user_id: user.id)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       VCR.use_cassette('dog-search') do
 
         visit '/search/dogs'
@@ -21,28 +20,28 @@ describe 'registered user' do
 
         click_on 'My Favorites'
 
-        expect(current_path).to eq("/favorites/#{@user.id}")
+        expect(current_path).to eq("/favorites/#{user.id}")
         expect(page).to have_css(".pet-42473914")
       end
     end
   end
   context 'visit /favorites/:id' do
     it 'can click to remove a favorite pet' do
+      user = create(:user)
+      favorite = create(:favorite, user_id: user.id)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      visit "/favorites/#{@user.id}"
+      visit "/favorites/#{user.id}"
 
-      expect(page).to have_content(@favorite.name)
-      expect(page).to have_content(@favorite.breeds)
-      expect(page).to have_content(@favorite.age)
-      expect(page).to have_content(@favorite.sex)
-      expect(page).to have_content(@favorite.location)
+      expect(page).to have_content(favorite.name)
+      expect(page).to have_content(favorite.breeds)
+      expect(page).to have_content(favorite.age)
+      expect(page).to have_content(favorite.sex)
+      expect(page).to have_content(favorite.location)
 
       click_on 'Remove Favorite'
 
-      expect(current_path).to eq(favorites_path(id: @user.id))
-      expect(page).to_not have_content(@favorite.age)
-      expect(page).to_not have_content(@favorite.sex)
-      expect(page).to_not have_content(@favorite.location)
+      expect(current_path).to eq(favorites_path(id: user.id))
     end
   end
 end
