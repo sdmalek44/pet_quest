8866cde6ab78d65d07f4a4727796f83f9cd1ad00
Stephen Malek <malek.stephen44@gmail.com>
2018-09-18 19:15:37 -0600

    make new pets job iterate through all users and send emails

--- app/jobs/new_pets_job.rb
+++ app/jobs/new_pets_job.rb
@@ -1,7 +1,9 @@
 class NewPetsJob < ApplicationJob
   queue_as :default
 
-  def perform(user)
-    UserNotifierMailer.send_new_pet_email(JobCreator.new(user)).deliver_now
+  def perform
+    User.all.each do |user|
+      UserNotifierMailer.send_new_pet_email(JobCreator.new(user)).deliver_now
+    end
   end
 end
