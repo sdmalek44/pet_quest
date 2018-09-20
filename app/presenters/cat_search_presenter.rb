e5c74dad1c898ba692d400d9a014d86f8f6521e4
Stephen Malek <malek.stephen44@gmail.com>
2018-09-20 10:16:31 -0600

    hardcode lat long values for presentation and make cook javascripty buttons

--- app/presenters/cat_search_presenter.rb
+++ app/presenters/cat_search_presenter.rb
@@ -1,5 +1,4 @@
 class CatSearchPresenter < BasePresenter
-  attr_reader :service
 
   def initialize(user, param_info = {})
     @service = PetfinderService.new(param_info)
@@ -13,4 +12,6 @@ class CatSearchPresenter < BasePresenter
   def breeds
     service.breeds('cat').map {|breed_info| Breed.new(breed_info[:$t])}
   end
+  
+  attr_reader :service
 end
